fc52ea94ec5e973aeff563bf7352f7b7acd41f31
RichieSingh <d.grewal86@hotmail.com>
2024-05-16 20:32:49 -0400

    Update query_3.sql

--- submission/query_3.sql
+++ submission/query_3.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE TABLE richiesingh.actors_history_scd(
+CREATE OR REPLACE TABLE actors_history_scd(
 actor VARCHAR, -- 'actor': Stores the actor's name. Part of the actor_films dataset.
 quality_class VARCHAR, --'quality class': Categorical rating based on average rating in the most recent year.
 is_active BOOLEAN, --'is_active': Indicates if the actor is currently active, based on making films this year.
